67057df56e9a6210a5374a746932b5fbfba663a1
anhvd <anhvdse04605@fpt.edu.vn>
2018-06-05 08:50:49 +0700

    edit

--- M101P_MongoDB-for-Devlelopers/Chapter 1/bottle_server.py
+++ M101P_MongoDB-for-Devlelopers/Chapter 1/bottle_server.py
@@ -1,7 +1,27 @@
+from pymongo import MongoClient
 from bottle import route, run, template
+
 
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
+
+
+@route('/')
+def index():
+    # connect to mongoDB
+    connection = MongoClient('localhost', 27017)
+
+    # attach to the database
+    db = connection.test
+
+    # get the names collection
+    names = db.names
+
+    # find a single document
+    item = names.find_one()
+    
+    return '<b>Hello {name}</b>'.format(name=item['name'])
+
 
 run(host='localhost', port=8080)
